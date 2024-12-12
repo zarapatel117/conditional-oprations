4fc404908ded66c736d5a341b2cb7b7c31c97667
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-12 20:47:41 +0530

    proffit or loss

---
+++ profit or loss.py
@@ -0,0 +1,8 @@
+num=float(input("Enter your selling price"))
+acutal_amount=float(input("Enter your acual amount"))
+if num>acutal_amount:
+    amount=num-acutal_amount
+    print("this is the profit",amount)
+
+else :
+    print("your in loss")    
